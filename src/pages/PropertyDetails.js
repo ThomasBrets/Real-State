62334420e387f9e8295be6326da9cbac8686b6ed
ThomasBrets <thomas15501331@gmail.com>
2023-11-01 22:33:45 -0300

    finished handleClick and start propertyDetails

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useContext } from "react";
+
+// Data
+import { housesData } from "../data";
+
+// Context
+import { HouseContext } from "../HouseContext/HouseContext";
+
+// useParams
+import { useParams, Link } from "react-router-dom";
+
+// Icons
+import { BiBed, BiBath, BiArea } from "react-icons/bi";
 
 const PropertyDetails = () => {
+  const { houses } = useContext(HouseContext);
+
+  // Get the house id
+  const { id } = useParams();
+  console.log("ID", id);
+
+  // Get house based on the id
+  const house = houses.find((house) => {
+    return house.id === parseInt(id);
+  });
+
+  console.log("HOUSE", house);
+
   return <div>PropertyDetails</div>;
 };
 
