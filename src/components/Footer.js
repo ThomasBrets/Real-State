38bd2294292d37eb39e5560ff2bc64dfbe84aaa7
ThomasBrets <thomas15501331@gmail.com>
2023-11-02 16:49:48 -0300

    finish Real State

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+// logo
+import Logo from "../assets/img/logo.svg"
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer className="text-white bg-black text-center py-8">
+    <div className="container mx-auto">
+      <div className="flex justify-between items-center">
+      <div className="bg-white p-4 rounded-lg">
+        <img src={Logo} alt="" />
+      </div>
+      <div>
+      Copyright &copy; 2023. All rights reserved.
+      </div>
+      </div>
+    </div>
+  </footer>;
 };
 
 export default Footer;
