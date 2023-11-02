ce2b2d4757ca7ae5b862e490ae0d6b8170ae3d01
ThomasBrets <thomas15501331@gmail.com>
2023-11-02 16:54:09 -0300

    RealState finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 // logo
 import Logo from "../assets/img/logo.svg"
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return <footer className="text-white bg-black text-center py-8">
     <div className="container mx-auto">
       <div className="flex justify-between items-center">
+     <Link to="/">
       <div className="bg-white p-4 rounded-lg">
         <img src={Logo} alt="" />
       </div>
+     </Link>
       <div>
       Copyright &copy; 2023. All rights reserved.
       </div>
