fd05587fc7137a37b572a3a02d615aae4499525e
ThomasBrets <thomas15501331@gmail.com>
2023-11-10 14:55:33 -0300

    col footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return <footer className="text-white bg-black text-center py-8">
     <div className="container mx-auto">
-      <div className="flex justify-between items-center">
+      <div className="flex flex-col lg:flex-row gap-y-2 lg:gap-y-0 justify-between items-center">
      <Link to="/">
       <div className="bg-white p-4 rounded-lg">
         <img src={Logo} alt="" />
